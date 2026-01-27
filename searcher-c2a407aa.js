7e031c5445b8002b03cb2652fde585b963c8479b
mgi388 <mgi388@users.noreply.github.com>
2026-01-27 06:03:41 +0000

    deploy: 7401da44eefc831f354ea47fe04830455d6a7a81

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3f5aee1f.js',
+                path_to_root + 'searchindex-bb5032a7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
